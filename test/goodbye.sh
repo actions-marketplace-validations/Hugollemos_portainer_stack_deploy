7990b105c0c985a2bd98ae03e1dbee9d7fdf3635
hugo lemos <hugolemosdasilva.hu@gmail.com>
2023-08-22 20:07:56 -0300

    push

--- test/goodbye.sh
+++ test/goodbye.sh
@@ -2,22 +2,77 @@
 URL=$API_URL
 API_KEY=$API_KEY
 FILE_PATH=$stack
+name=$name
 
-response=$(curl -s -X GET $URL -H "X-API-Key: $API_KEY" --insecure)
+# Faz a solicitação GET e armazena a resposta em uma variável
+response=$(curl -s -X GET "$URL" -H "X-API-Key: $API_KEY" --insecure)
 
-# Verifica se a stack com nome "ubuntu" está criada
-if echo "$response" | jq -e '.[] | select(.Name == "front")' > /dev/null; then
-  # Extrai o valor do campo "Name" da resposta usando jq
-  name=$(echo "$response" | jq -r '.[] | select(.Name == "front") | .Name')
+# Verifica se a stack está criada
+if echo "$response" | jq -e '.[] | select(.Name == "'"$name"'")' > /dev/null; then
+
+  # Extrai o valor do campo "Name" usando jq
+  name=$(echo "$response" | jq -r '.[] | select(.Name == "'"$name"'") | .Name')
 
   # Imprime o nome da stack
-  echo "A Stack chamada $name está criada. Nome: $name"
+  echo "A Stack chamada $name está criada."
 
-  # Extrai o valor do campo "Id" da resposta usando jq
-  id=$(echo "$response" | jq -r '.[] | select(.Name == "front") | .Id')
-
+  # Obtém o ID da stack
+  id=$(echo "$response" | jq -r '.[] | select(.Name == "'"$name"'") | .Id')
+  
   # Imprime o ID da stack
   echo "Obtendo ID da stack: $id"
+  
+  # Monta a URL para a exclusão
+  DELETE_URL="$URL/$id"
+  
+  # Faz a solicitação DELETE para atualizar a stack
+  curl -X DELETE "$DELETE_URL" \
+  -H "X-API-Key: $API_KEY" \
+  -F "type=2" \
+  -F "method=file" \
+  -F "file=@$FILE_PATH" \
+  -F "endpointId=2" \
+  -F "Name="$name" 
+
+  echo "Stack deletada. ID: $id"
+  echo "stop 6 sec"
+  sleep 6
+  echo "CRIANDO A STACK $name"
+
+  response=$(curl -s -X POST "$URL" \
+  -H "X-API-Key: $API_KEY" \
+  -F "type=2" \
+  -F "method=file" \
+  -F "file=@$FILE_PATH" \
+  -F "endpointId=2" \
+  -F "Name="$name" )
+
+  # Imprimir a resposta da requisição 
+  echo "Resposta da solicitação POST: $response"
+
+  # Extrair o valor do campo "Id" da nova stack usando jq
+  id=$(echo "$response" | jq -r '.Id')
+
+  # Imprimir o valor do Id
+  echo "Nova Stack criada. Id: $id"
 else
-  echo "Nenhuma Stack com nome $name encontrada."
+  echo "Nenhuma Stack da aplicação encontrada."
+  echo "CRIANDO A NOVA STACK"
+
+  response=$(curl -s -X POST "$URL" \
+  -H "X-API-Key: $API_KEY" \
+  -F "type=2" \
+  -F "method=file" \
+  -F "file=@$FILE_PATH" \
+  -F "endpointId=2" \
+  -F "Name="$name" )
+
+  # Imprimir a resposta da requisição 
+  echo "Resposta da solicitação POST: $response"
+
+  # Extrair o valor do campo "Id" da nova stack usando jq
+  id=$(echo "$response" | jq -r '.Id')
+
+  # Imprimir o valor do Id
+  echo "Nova Stack criada. Id: $id"
 fi
